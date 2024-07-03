c9647a1b2cf9cd17fcc23b5ba54cd83fac253a3e
aixion1506 <aixion1506@gmail.com>
2024-07-03 15:57:45 +0900

    express, ejs 설치

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Sever is running! The port number is ${port} ...`);
+})
