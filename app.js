8e9f470ea14b14262086ccdcd0612f1879187a79
eungyeong0927 <bb060@naver.com>
2024-07-09 16:43:50 +0900

    Fix: 포트 번호 변경

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.listen(PORT, () => {
-    console.log(`Sever is running! The port number is ${port} ...`);
-})
+  console.log(`Sever is running! The port number is ${PORT} ...`);
+});
