e1cc41cd52ad7f217b53c6ee4fd0ea8322fd2499
aixion <aixion1506@gmail.com>
2024-07-09 16:33:10 +0900

    Feat: UserModel 정의

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        userId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        userName: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        loginId: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        userPw: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(30),
+            allowNull: true
+        },
+        address: {
+            type: DataTypes.STRING(50),
+            allowNull: true
+        },
+        profileImg: {
+            type: DataTypes.STRING(100),
+            allowNull: true
+        },
+        userNick: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        birthday: {
+            type: DataTypes.DATEONLY,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.NOW
+        },
+        updateAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: Sequelize.NOW
+        }
+    }, {
+        freezeTableName: true
+    })
+
+    return User
+}
+
+module.exports = UserModel;
