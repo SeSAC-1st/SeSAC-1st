2f9e6d26164c07ddfe1ceec54a9fff35d434c69b
youna99 <dbsk0108@gmail.com>
2024-07-10 19:05:12 +0900

    Add: app.js에 static 폴더 경로 설정 추가

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('static')); // static폴더를 내가 static파일을 가져오는 폴더로 등록한다
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(PORT, () => {
   console.log(`Sever is running! The port number is ${PORT} ...`);
