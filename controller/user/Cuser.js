fc79f086770bbc2d17fe5376014e8b886d94e1a3
aixion <aixion1506@gmail.com>
2024-07-10 18:09:56 +0900

    Fix: User 모델 import 경로 수정

--- controller/user/Cuser.js
+++ controller/user/Cuser.js
@@ -1,4 +1,4 @@
-const { User } = require('../../models/user/User');
+const { User } = require('../../models/index');
 
 // 회원가입 페이지
 exports.registerPage = (req, res) => {
@@ -8,6 +8,7 @@ exports.registerPage = (req, res) => {
 // 회원가입 로직
 exports.userRegister = async (req, res) => {
     try {
+        console.log('User ->', User);
         console.log(req.body)
         const {
             userName, loginId, userPw, email, address, profileImg, userNick, birthday
@@ -17,6 +18,20 @@ exports.userRegister = async (req, res) => {
         });
 
         res.json(newUser);
+        console.log(newUser)
+        // {
+        //     "userId": 1,
+        //     "userName": "ranis",
+        //     "loginId": "ranikuns",
+        //     "userPw": "rani12345",
+        //     "email": "rani@gamil.com",
+        //     "address": "seoul",
+        //     "profileImg": "cat",
+        //     "userNick": "babocat",
+        //     "birthday": "1998-06-15",
+        //     "updatedAt": "2024-07-10T08:34:58.834Z",
+        //     "createdAt": "2024-07-10T08:34:58.834Z"
+        // }
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -27,25 +42,3 @@ exports.userRegister = async (req, res) => {
 exports.loginPage = (req, res) => {
     res.render('/user/loginPage');
 }
-
-// 로그인 로직
-exports.userLogin = async (req, res) => {
-    try {
-        console.log(req.body);
-        const { userid, userPw } =req.body;
-        
-        const user = await User.findOne({
-            where: { userid, userPw },
-        });
-
-        res.json(user);
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-}
-
-// 로그아웃 페이지
-
-// 로그아웃 로직
