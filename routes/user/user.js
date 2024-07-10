2eee091025b08f33ee57874d1ffda7cbbcb52eef
aixion <aixion1506@gmail.com>
2024-07-10 18:11:09 +0900

    Feat: 로그인 페이지, 로직 route

--- routes/user/user.js
+++ routes/user/user.js
@@ -8,4 +8,10 @@ router.get('/register', controller.registerPage);
 // 회원가입 로직
 router.post('/register', controller.userRegister);
 
+// 로그인 페이지
+router.get('/login', controller.loginPage);
+
+// 로그인 로직
+router.post('/login', controller.userLogin);
+
 module.exports = router;
