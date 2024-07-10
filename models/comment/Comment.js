763ead51b5f00896abe799417eb12cca59245add
qldirr <sub05073@naver.com>
2024-07-10 16:19:13 +0900

    Feat: 댓글 관련 model, controller, router 최종 변경

--- models/comment/Comment.js
+++ models/comment/Comment.js
@@ -1,68 +1,53 @@
 const commentModel = (sequelize, DataTypes) => {
-    const Comment = sequelize.define('Comment', {
-        comId: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-        //   comment: '댓글 아이디'
-        },
-        comContent: {
-          type: DataTypes.STRING(200),
-          allowNull: false,
-        //   comment: '댓글 내용'
-        },
-        postId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        //   comment: '게시판 아이디',
-        //   references: {
-        //     model: 'Post',
-        //     key: 'id'
-        //   }
-        },
-        userId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        //   comment: '회원 아이디',
-        //   references: {
-        //     model: 'User',
-        //     key: 'id'
-        //   }
-        },
-        // createdAt: {
-        //   type: DataTypes.DATE,
-        //   allowNull: false,
-        //   defaultValue: Sequelize.NOW,
-        //   comment: '생성일자'
+  const Comment = sequelize.define(
+    'Comment',
+    {
+      comId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      comContent: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+      postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        // references: {
+        //   model: 'Post',
+        //   key: 'postId',
         // },
-        // updatedAt: {
-        //   type: DataTypes.DATE,
-        //   allowNull: false,
-        //   defaultValue: Sequelize.NOW,
-        //   comment: '수정일자'
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        // references: {
+        //   model: 'User',
+        //   key: 'userId',
         // },
-        parentComId: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-        //   comment: '상위 댓글 아이디',
-        //   references: {
-        //     model: 'Comment',
-        //     key: 'comId'
-        //   }
-        },
-        isDeleted: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue: false,
-        //   comment: '삭제 여부'
-        }
-      }, {
-        freezeTableName: true
-        // timestamps: false
-      });
+      },
+      parentComId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        // references: {
+        //   model: 'Comment',
+        //   key: 'comId',
+        // },
+      },
+      isDeleted: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      freezeTableName: true,
+    }
+  );
 
-    return Comment
-}
+  return Comment;
+};
 
-module.exports = commentModel
+module.exports = commentModel;
--- routes/comment/comment.js
+++ routes/comment/comment.js
@@ -3,18 +3,21 @@ const controller = require('../../controller/comment/Ccomment');
 const router = express.Router();
 
 //댓글, 대댓글 목록 조회
-router.get('/list/:postId', controller.getCommentList)
+router.get('/list/:postId', controller.getCommentList);
 
 // 댓글 등록
-router.post('/:postId', controller.insertComment)
+router.post('/:postId', controller.insertComment);
 
 // 댓글, 대댓글 수정
-router.patch('/update/:comId', controller.updateComment)
+router.patch('/update/:comId', controller.updateComment);
 
-// 댓글, 대댓글 삭제
-router.patch('/delete/:comId', controller.deleteComment)
+// 댓글 삭제
+router.patch('/delete/:comId', controller.deleteComment);
+
+// 대댓글 삭제
+router.patch('/reply/delete/:comId', controller.deleteCommentReply);
 
 // 대댓글 등록
-router.post('/:comId', controller.insertReply)
+router.post('/reply/:comId', controller.insertReply);
 
-module.exports = router
+module.exports = router;
