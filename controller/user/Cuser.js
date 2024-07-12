ef16d1483d7b7ca90146bec36174ece3247e3c60
aixion <aixion1506@gmail.com>
2024-07-12 11:39:23 +0900

    Fix: Register data 변경

--- controller/user/Cuser.js
+++ controller/user/Cuser.js
@@ -1,19 +1,18 @@
 const { User } = require('../../models/index');
-const { hashPw } = require('../../utils/bcrypt');
+const { hashPw, comparePw } = require('../../utils/bcrypt');
 
 // 회원가입 페이지
 exports.registerPage = (req, res) => {
     res.render('/user/registerPage');
-  };
+};
 
 // 회원가입 로직
-// 회원가입 암호화
 exports.userRegister = async (req, res) => {
     try {
         console.log('User ->', User);
         console.log(req.body)
         const {
-            userName, loginId, userPw, email, address, profileImg, userNick, birthday
+            userName, loginId, userPw, email, address, userNick, birthday
         } = req.body;
 
          // userName 정규표현식 검사 (한글, 영어로 2~10글자)
@@ -22,7 +21,7 @@ exports.userRegister = async (req, res) => {
              return res.status(400).json({ error: 'Invalid userName.' });
          }
 
-        // loginId 정규표현식 (6~10글자, 영어 대소문자)
+        // loginId 정규표현식 (6~10글자, 영어 대소문자, 숫자 포함)
         const loginIdRegex = /^[a-zA-Z0-9]{6,10}$/;
         if (!loginIdRegex.test(loginId)) {
             return res.status(400).json({ error: 'Invalid loginId.' });
@@ -44,7 +43,7 @@ exports.userRegister = async (req, res) => {
         const hashedPw = hashPw(userPw);
 
         const newUser = await User.create({
-            userName, loginId, userPw: hashedPw, email, address, profileImg, userNick, birthday
+            userName, loginId, userPw: hashedPw, email, address, userNick, birthday
         });
 
         res.json(newUser);
@@ -74,10 +73,19 @@ exports.loginPage = (req, res) => {
 }
 
 
+/**
+ * 사용자의 로그인 요청을 처리합니다.
+ * 
+ * @param {Object} req - Express 요청 객체
+ * @param {Object} req.body - 요청 본문
+ * @param {string} req.body.loginId - 사용자의 로그인 아이디 (6~10글자, 영어 대소문자)
+ * @param {string} req.body.userPw - 사용자의 비밀번호 (8~12글자, 영어, 숫자, 특수문자 포함)
+ * @param {Object} res - Express 응답 객체
+ * @returns {Promise<void>} - 비동기 함수이므로 Promise를 반환합니다.
+ */
 
 // 로그인 로직
 
-// 로그인 암호화
 // 로그인 암호화 DB 대조
 // 로그인 session
 exports.userLogin = async (req, res) => {
@@ -99,9 +107,13 @@ exports.userLogin = async (req, res) => {
         console.log('loginId ->', loginId);
         console.log('userPw ->', userPw);
 
+        // 비밀번호 해싱
+        const hashedPw = hashPw(userPw);
+
+        const comparedPw = comparePw()
 
         const user = await User.findOne({
-            where: { loginId, userPw },
+            where: { loginId, userPw:hashedPw },
         });
 
         res.json(user);
