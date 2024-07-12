9f4987adb4ed46441be391d0e861d6a26da2d3a4
aixion1506 <aixion1506@gmail.com>
2024-07-12 14:22:15 +0900

    Feat: session middleware

---
+++ middlewares/session.js
@@ -0,0 +1,16 @@
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.resolve(__dirname, '.env'),
+}); 
+
+const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // 개발 중에는 false, 프로덕션에서는 true로 설정
+})
+
+module.exports = sessionMiddleware;
