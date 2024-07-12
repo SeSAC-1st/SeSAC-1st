05761f0a19f652113f69006fc4e4553b62e651d6
aixion1506 <aixion1506@gmail.com>
2024-07-12 15:13:06 +0900

    Fix: env 파일 경로 변경

--- middlewares/session.js
+++ middlewares/session.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-    path: path.resolve(__dirname, '.env'),
+    path: path.resolve(__dirname, '../.env'),
 }); 
 
 const sessionMiddleware = session({
