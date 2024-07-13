98e9c2cddf90c29854260790e7e98ef74ab17c1f
aixion1506 <aixion1506@gmail.com>
2024-07-13 15:30:18 +0900

    Feat: Session 보안 강화

--- middlewares/session.js
+++ middlewares/session.js
@@ -9,8 +9,12 @@ dotenv.config({
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // 개발 중에는 false, 프로덕션에서는 true로 설정
+    saveUninitialized: false, // 권장 설정: true로 하면 로그인하지 않아도 세션이 생성됨
+    cookie: { 
+        secure: false , // 개발 중에는 false, 프로덕션에서는 true로 설정
+        httpOnly: true, // 클라이언트에서 쿠키에 접근하지 못하도록 설정
+        maxAge: 1000 * 60 * 60 * 24 // 쿠키 유효기간 설정 (예: 1일)
+    } 
 })
 
 module.exports = sessionMiddleware;
